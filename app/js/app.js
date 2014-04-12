878d9785c15a157418d70aedbd1a910f7e6f586a
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-04-11 23:06:32 -0500

    update

--- app/js/app.js
+++ app/js/app.js
@@ -4,21 +4,18 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
 	'ngRoute',
+	'firebase',
 	'myApp.filters',
 	'myApp.services',
 	'myApp.directives',
 	'myApp.controllers'
 	]).
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', {
-		templateUrl: 'partials/partial1.html', 
-		controller: 'MyCtrl1'
-	});
-	$routeProvider.when('/view2', {
-		templateUrl: 'partials/partial2.html', 
-		controller: 'MyCtrl2'
+	$routeProvider.when('/Private', {
+		templateUrl: 'partials/private.html', 
+		controller: 'PrivateCtrl'
 	});
 	$routeProvider.otherwise({
-		redirectTo: '/view1'
+		redirectTo: '/Private'
 	});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,47 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var url = "https://meetup2014.firebaseIO.com";
 
-  }])
-  .controller('MyCtrl2', [function() {
+angular.module('myApp.controllers', ['firebase'])
+.controller('userCtrl', ["$scope", "$rootScope", "$firebase", "$firebaseSimpleLogin", function($scope, $rootScope, $firebase,$firebaseSimpleLogin) {
+	
+	var ref = new Firebase(url+"/Users");
+	$scope.users = $firebase(ref);
+	$scope.auth = $firebaseSimpleLogin(ref);
 
-  }]);
+	$scope.login = function(){
+		$scope.auth.$login('facebook', {rememberMe: false, scope:'email,basic_info'});
+		$rootScope.id = $scope.auth.user.id;
+		if (!$scope.users.$child(id)){
+			alert("hello");
+			upload();
+		}
+	}
+
+	function upload(){
+		var email = $scope.auth.user.email;
+		var pic = "https://graph.facebook.com/{{auth.user.id}}/picture";
+		var name = $scope.auth.user.name;
+		$scope.users.$child(id).$set({email:email, pic:pic, Username: name});
+	};
+
+}])  
+.controller('PrivateCtrl', ["$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase) {
+
+	$rootScope.$watch('auth.user', getEvents);
+
+	if ($rootScope.auth.user){
+		getEvents();
+	}
+
+	function getEvents(){
+		$scope.privateE = $rootScope.root.$child('Private')
+		$scope.events = $scope.privateE.$child('UserID');
+	}
+
+
+}])
+.controller('MyCtrl2', [function() {
+
+}]);
